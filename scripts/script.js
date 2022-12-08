20e571cdaa27b75ca6beaf71e75a510d53067852
Romi Snijders <romi.snijders@hotmail.com>
2022-12-08 12:46:50 +0100

    comments added

--- scripts/script.js
+++ scripts/script.js
@@ -5,16 +5,21 @@ function assignLocation(page) {
 
 //Used this tutorial to make hamburger menu https://www.youtube.com/watch?v=flItyHiDm7E
 
+
+//get your HTML elements to use in javascript
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLink = document.querySelectorAll(".nav-link");
 
+
+//to listen to an event happing on the part mentioned before (changing hamburger menu to cross)
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
   document.querySelector("main").classList.toggle("active");
 });
 
+//listens to each item in the hamburger menu
 navLink.forEach((item) => {
   item.addEventListener("click", () => {
     hamburger.classList.remove("active");
@@ -22,24 +27,22 @@ navLink.forEach((item) => {
   });
 });
 
-
+//to change the total price regarding the number of items
 const inputNumber = document.querySelector("input");
 const totalPrice = document.querySelector(".total-price");
 
 function changePrice() {
-  if (inputNumber.value > 0) {
-    totalPrice.innerHTML = "€ " + inputNumber.value + "0"
-  } else {
-    totalPrice.innerHTML = "€ 0"
-  }
+  totalPrice.innerHTML = "€ " + inputNumber.value * 10
 }
 
+
+//to make the basket shake
 const addToCart = document.querySelector(".add-button");
 const basketLogo = document.querySelector(".basketlogo");
 
 addToCart.addEventListener("click", () => {
-  basketLogo.classList.add("shake");
-  setTimeout(function () {
+  basketLogo.classList.add("shake"); //add the shake class
+  setTimeout(function () { //make the shake class be removed after 1sec so you can shake it again 
     basketLogo.classList.remove('shake');
   }, 1100);
 })
